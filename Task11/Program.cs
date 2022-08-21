97843c1f53cd8e6e8bcd6b755b3830573965708d
Евгений Долматов <C:\Users\datat\AppData\Roaming\The Bat!>
2022-08-22 00:01:01 +0700

    задачи

---
+++ Task11/Program.cs
@@ -0,0 +1,30 @@
+﻿// 11. Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+// int number = new Random().Next(100, 1000); //10, 99 + 1
+//  Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+
+//  int firstDigit = number / 100;
+//  int secondDigit = number % 10;
+//  int nov = firstDigit*10+secondDigit;
+//  Console.WriteLine($"{number} => {nov} ");
+
+// С помощью метода:
+
+int number = new Random().Next(100, 1000); //10, 99 + 1
+Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
+
+int RemoveSecond(int num)
+{
+ int firstDigit = num / 100;
+ int secondDigit = num % 10;
+ int nov = firstDigit*10+secondDigit;
+ Console.WriteLine($"{number} => {nov} ");
+ return nov;
+ }
+ int result = RemoveSecond(number);
+ Console.WriteLine($"{number} => {result} ");
---
+++ Task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Multip(int num, int a, int b)
+{
+    return num % a == 0 && num % b == 0;
+}
+if (Multip(number, 7, 23)) Console.WriteLine($"{number} -> Да");
+else Console.WriteLine($"{number} -> Нет");
